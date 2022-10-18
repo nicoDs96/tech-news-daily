13355903a5daf148bbc401de4544a358fb42f9a1
Eugenio Del Male <nicola96ds@gmail.com>
2022-10-18 11:58:14 +0200

    news aggregator init commit

---
+++ news-aggregator/src/main/java/ds/nico/aggregator/FeedResource.java
@@ -0,0 +1,27 @@
+package ds.nico.aggregator;
+
+import io.quarkus.panache.common.Page;
+import io.quarkus.panache.common.Sort;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("feeds")
+public class FeedResource {
+    @ConfigProperty(name = "news.aggregator.page.size")
+    Integer pageSize;
+    Integer pageSizeDefault = 30;
+
+    @GET
+    @Path("/{idx}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<FeedBean> getPage(Integer idx) {
+        Integer ps = pageSize == null? pageSizeDefault : pageSize;
+        return FeedBean.findAll(Sort.by("updateDate", "date").descending()).page(Page.of(idx, ps)).list();
+    }
+
+}
