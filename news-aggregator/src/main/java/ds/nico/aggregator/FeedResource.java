ffc1f4bce97ea0a4e57516c6d24ad4911fa7ce92
Eugenio Del Male <nicola96ds@gmail.com>
2022-10-19 13:28:13 +0200

    add startup logs

--- news-aggregator/src/main/java/ds/nico/aggregator/FeedResource.java
+++ news-aggregator/src/main/java/ds/nico/aggregator/FeedResource.java
@@ -1,9 +1,12 @@
 package ds.nico.aggregator;
 
+import io.quarkus.logging.Log;
 import io.quarkus.panache.common.Page;
 import io.quarkus.panache.common.Sort;
+import io.quarkus.runtime.StartupEvent;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.enterprise.event.Observes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -15,6 +18,12 @@ public class FeedResource {
     @ConfigProperty(name = "news.aggregator.page.size")
     Integer pageSize;
     Integer pageSizeDefault = 30;
+    @ConfigProperty(name = "quarkus.mongodb.connection-string")
+    String mongoString;
+    void onStart(@Observes StartupEvent ev) {
+        Log.info("The mongo string is: " + mongoString);
+        Log.info("Page size is: " + pageSize);
+    }
 
     @GET
     @Path("/{idx}")
